0dab5252523bb1fbdf435a4fe42d644157439d0e
zjr <zhangjingrui@pekon.com>
2021-03-05 23:25:36 +0800

    commit

--- sentinel-demo/sentinel-demo-spring-webmvc/src/main/java/com/alibaba/csp/sentinel/demo/spring/webmvc/WebMvcDemoApplication.java
+++ sentinel-demo/sentinel-demo-spring-webmvc/src/main/java/com/alibaba/csp/sentinel/demo/spring/webmvc/WebMvcDemoApplication.java
@@ -28,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebMvcDemoApplication {
 
     public static void main(String[] args) {
+        //设置configuration
         SpringApplication.run(WebMvcDemoApplication.class);
     }
 }
